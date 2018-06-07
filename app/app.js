9de5c26b19650803027832b09ecf314be0c196f0
Svetozar Toskov <s.toskov@gmail.com>
2018-06-07 15:41:10 +0300

    Remove views from app.js

--- app/app.js
+++ app/app.js
@@ -1,32 +1,7 @@
 import React from "react";
 import {createStackNavigator} from "react-navigation";
-import {StyleSheet, Text, View, Button} from 'react-native';
 import Cars from "./views/cars/cars";
 import CarDetails from "./views/car-details/cars-details";
-
-class HomeScreen extends React.Component {
-    render() {
-        return (
-            <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-                <Text>Home Screen 12</Text>
-                <Button
-                    title="Go to Details"
-                    onPress={() => this.props.navigation.navigate('Details')}
-                />
-            </View>
-        );
-    }
-}
-
-class DetailsScreen extends React.Component {
-    render() {
-        return (
-            <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-                <Text>Details Screen</Text>
-            </View>
-        );
-    }
-}
 
 const RootStack = createStackNavigator(
     {
